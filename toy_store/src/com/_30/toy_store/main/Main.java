3366ac2946a4ff028500bb0bde997a8b096b40a2
merge-simpson <letsdev@kakao.com>
2021-08-31 16:56:19 +0900

    [Update] (Main.java) File Path 명시(using NTSytem)

--- toy_store/src/com/_30/toy_store/main/Main.java
+++ toy_store/src/com/_30/toy_store/main/Main.java
@@ -5,8 +5,14 @@ import com._30.toy_store.ui.ToyStoreUI;
 public final class Main {
 
 	public static void main(String[] args) {
-		// FIXME 파일 경로
-		String filePath = "파일경로.txt";
+		// 운영체제 사용자
+		com.sun.security.auth.module.NTSystem NTSystem = new
+		        com.sun.security.auth.module.NTSystem();
+		String pcUserName = NTSystem.getName();
+		
+		// .dat 대신 .txt 사용
+		String filePath = String.format(
+				"C:\\users\\%s\\toy-store-data\\objects.txt", pcUserName);
 		ToyStoreUI ui = new ToyStoreUI(filePath);
 		ui.start();
 	}
